a18c84be1649bbecb30c1c189364ddbc397a1aab
Coopermakc <Coopermakc@mail.ru>
2020-07-31 11:46:41 +0300

    allowed host

--- my_site/settings.py
+++ my_site/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
